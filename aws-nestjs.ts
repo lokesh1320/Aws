d06e349c9cfb00fae6b3b74693646d39f1694eac
Lokesh Kuruva <lokesh.kuruva@expleogroup.com>
2023-11-14 15:55:28 +0530

    basic operations of S3

--- aws-nestjs.ts
+++ aws-nestjs.ts
@@ -1,4 +1,4 @@
-import { GetObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3"
+import { DeleteObjectsCommand, GetObjectCommand, ListObjectsV2Command, PutObjectCommand, S3Client, } from "@aws-sdk/client-s3"
 import { Injectable } from "@nestjs/common"
 
 const s3Client = new S3Client({
